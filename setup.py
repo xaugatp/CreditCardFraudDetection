bedb164371fdd22f35564288eee51e43a425a24e
xaugatp <saugatp363@gmail.com>
2024-08-09 18:44:48 +1000

    first commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT ='-e'
+
+
+def get_requirements(file_path:str)-> List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements =file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+        
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+        
+        return requirements
+
+
+setup(
+    name='Credit Card Fraud Detection',
+    version='0.0.1',
+    author='Saugat Poudel',
+    author_email='saugat.poudel@student.torrens.edu.au',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+    
+    )
